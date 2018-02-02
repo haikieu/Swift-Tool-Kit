b24878cdb7ef3632a34c23271dc74da74b8315c8
Hai Kieu <haikieu2907@gmail.com>
2018-02-01 18:08:29 -0800

    Add Screens

---
+++ TookKitDemo/ToolKitMacApp/Screens/Console/ConsoleViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ConsoleViewController.swift
+//  ToolKitMacApp
+//
+//  Created by KIEU, HAI on 2/1/18.
+//  Copyright © 2018 haikieu2907@icloud.com. All rights reserved.
+//
+
+import Cocoa
+
+class ConsoleViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
